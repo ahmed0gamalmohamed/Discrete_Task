293eb2865790304cdcf88917c7bde1e4d22fd185
ahmed0gamalmohamed <121305720+ahmed0gamalmohamed@users.noreply.github.com>
2022-12-24 21:49:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("please enter the number smallest");
+double n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the number largest");
+double n2 = double.Parse(Console.ReadLine());
+
+for (double i = n1; i <= n2; i++)
+{
+    for (double y = 2; y <= i; y++)
+    {
+        if (i == 2)
+            Console.WriteLine(2);
+        if (i % y == 0)
+            break;
+        else if (y == i - 1)
+            Console.WriteLine(i);
+    }
+}
